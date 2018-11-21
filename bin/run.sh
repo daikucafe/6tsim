6e8f641ba731716ef2ecbea1b5d3e0caacb7f1fe
Marco Torres <marco.torres@me.com>
2018-11-21 02:51:46 -0300

    config for intermediate remote task and simData backuped outside git repo

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,4 @@
-python runSim.py --config 100-1.json &
-python runSim.py --config 100-2.json &
+python runSim.py --config 1-taof-per.json
+python runSim.py --config 2-of0-per.json
+python runSim.py --config 3-taof-var.json
+python runSim.py --config 4-of0-var.json
